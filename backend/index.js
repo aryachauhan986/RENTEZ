1d11092f4c19afd2880986e5efe72f3418a9d8c6
aryachauhan986 <91840680+aryachauhan986@users.noreply.github.com>
2025-08-31 15:04:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",   //to allow the request from this localhost too
+    origin:"https://rentez-frontend.onrender.com",   //to allow the request from this localhost too
     credentials:true
 }))
 
